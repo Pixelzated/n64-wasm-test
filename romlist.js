fbacc6e7a08fb6d0746d6196502ef8ff7e42bcdf
Pixelzated <104920477+Pixelzated@users.noreply.github.com>
2023-11-02 08:47:33 -0500

    Update romlist.js

--- romlist.js
+++ romlist.js
@@ -1,6 +1,6 @@
 var ROMLIST = [
     
-    {url:"roms/rom1",title:"Paper Mario"},
+    {url:"roms/rom1.n64",title:"Paper Mario"},
     /*
     {url:"roms/rom2.v64",title:"Game 2"},
     {url:"roms/rom3.v64",title:"Game 3"},
