6ded74a7baeb52d98137b1cbe1e7069953ca8ef5
Pixelzated <104920477+Pixelzated@users.noreply.github.com>
2024-05-10 12:07:14 -0500

    Update romlist.js
    removed paper mario

--- romlist.js
+++ romlist.js
@@ -1,7 +1,7 @@
 var ROMLIST = [
     
-    {url:"roms/rom1.n64",title:"Paper Mario"},
     /*
+    {url:"roms/rom1.n64",title:"Paper Mario"},
     {url:"roms/rom2.v64",title:"Game 2"},
     {url:"roms/rom3.v64",title:"Game 3"},
     */
